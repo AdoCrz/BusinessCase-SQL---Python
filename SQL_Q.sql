03e9f472ddf5298c529532f0aa950c7dcaa76836
AdoCrz <157544003+AdoCrz@users.noreply.github.com>
2024-04-22 12:54:59 -0600

    Create SQL_Q.sql

---
+++ SQL_Q.sql
@@ -0,0 +1,11 @@
+-- CREAR NUEVA BASE DE DATOS 
+
+CREATE DATABASE BD_Rappi;
+USE BD_Rappi;
+
+-- ¿Cuál es la distribución geográfica de las ventas de productos Nike durante el último trimestre del año 2023? --
+
+SELECT * FROM Products;
+SELECT * FROM Sales;
+SELECT * FROM Customers;
+SELECT * FROM Regions;
