e99af97853f71c006cb514550e07b4599e58b9ea
AdoCrz <157544003+AdoCrz@users.noreply.github.com>
2024-04-22 13:39:48 -0600

    Update SQL_8.sql

--- SQL_8.sql
+++ SQL_8.sql
@@ -4,3 +4,5 @@ DATENAME(WEEKDAY,date_sale) AS [Day],
 SUM(quantity) AS Quantity
 FROM Sales 
 GROUP BY DATENAME(WEEKDAY,date_sale);
+
+-- Los días sábado es donde más cantdidad de producto se compra, mientras que el día en que menos se compra es vierens
