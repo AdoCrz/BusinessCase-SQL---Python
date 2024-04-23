dcc64b63744a6c17df80a6ac20a51e4b755f39fc
AdoCrz <157544003+AdoCrz@users.noreply.github.com>
2024-04-22 21:48:53 -0600

    Update SQL_3.sql

--- SQL_3.sql
+++ SQL_3.sql
@@ -3,6 +3,26 @@ SELECT *  FROM Sales;
 SELECT * FROM Customers;
 SELECT * FROM Countries;
 
+
+
+WITH RankedProducts AS (
+    SELECT 
+        C.country,
+        CR.customer_name,
+        MONTH(S.date_sale) AS sale_month,
+        SUM(S.quantity) AS total_quantity,
+        ROW_NUMBER() OVER (PARTITION BY C.country, MONTH(S.date_sale) ORDER BY SUM(S.quantity) DESC) AS rank
+    FROM Countries AS C
+    INNER JOIN Customers AS CR ON C.id_country = CR.id_country
+    INNER JOIN Sales AS S ON CR.id_customer = S.id_customer
+    GROUP BY C.country, MONTH(S.date_sale), CR.customer_name
+)
+SELECT country, sale_month, customer_name, total_quantity
+FROM RankedProducts
+WHERE rank = 1;
+
+-- Para ver la lista completa
+
 SELECT CR.country,
 C.customer_name, 
 MONTH(S.date_sale) AS [Month],
